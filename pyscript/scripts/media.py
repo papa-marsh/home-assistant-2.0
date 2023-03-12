d17c72c0269f5a7c3123ff78537eb28e9c62cc38
papa-marsh <mwarners93@gmail.com>
2023-03-11 22:04:38 -0500

    Media card done

--- pyscript/scripts/media.py
+++ pyscript/scripts/media.py
@@ -7,11 +7,11 @@ def persist_media_card():
         "pyscript.media_card",
         default_value="controls",
         default_attributes={
-            "grouped": False,
-            "synced": False,
+            "group": False,
+            "sync": False,
             "group_icon": "mdi:speaker",
             "group_text": "Group",
-            "sync_icon": "mdi:music-box-multiple-outline",
+            "sync_icon": "mdi:volume-source",
             "sync_text": "Sync",
         },
     )
@@ -19,13 +19,12 @@ def persist_media_card():
 
 @service("lovelace.media_card_group")
 def media_card_group():
-    pyscript.media_card.grouped = not pyscript.media_card.grouped
+    pyscript.media_card.group = not pyscript.media_card.group
 
 
 @service("lovelace.media_card_sync")
 def media_card_sync():
-    # TODO
-    pyscript.media_card.synced = not pyscript.media_card.synced
+    pyscript.media_card.sync = not pyscript.media_card.sync
 
 
 @service("lovelace.media_card_more")
@@ -33,9 +32,9 @@ def media_card_more():
     pyscript.media_card = "volume" if pyscript.media_card == "controls" else "controls"
 
 
-@state_trigger("pyscript.media_card.grouped")
+@state_trigger("pyscript.media_card.group")
 def update_group_button():
-    if pyscript.media_card.grouped:
+    if pyscript.media_card.group:
         pyscript.media_card.group_icon = "mdi:speaker-multiple"
         pyscript.media_card.group_text = "Grouped"
     else:
@@ -43,21 +42,21 @@ def update_group_button():
         pyscript.media_card.group_text = "Group"
 
 
-@state_trigger("pyscript.media_card.synced")
+@state_trigger("pyscript.media_card.sync")
 def update_sync_button():
-    if pyscript.media_card.synced:
-        pyscript.media_card.sync_icon = "mdi:music-box-multiple"
+    if pyscript.media_card.sync:
+        pyscript.media_card.sync_icon = "mdi:volume-equal"
         pyscript.media_card.sync_text = "Synced"
     else:
-        pyscript.media_card.sync_icon = "mdi:music-box-multiple-outline"
+        pyscript.media_card.sync_icon = "mdi:volume-source"
         pyscript.media_card.sync_text = "Sync"
 
 
-@state_trigger("media_player.living_room.group_members", "pyscript.media_card.grouped")
+@state_trigger("media_player.living_room.group_members", "pyscript.media_card.group")
 def join_to_group():
-    if pyscript.media_card.grouped and len(
-        media_player.living_room.group_members
-    ) < len(constants.SPEAKER_GROUP):
+    if pyscript.media_card.group and len(media_player.living_room.group_members) < len(
+        constants.SPEAKER_GROUP
+    ):
         group_speakers()
 
 
@@ -66,11 +65,19 @@ def join_to_group():
     "media_player.front_room.volume_level",
     "media_player.craft_room.volume_level",
     "media_player.basement.volume_level",
+    "pyscript.media_card.sync",
 )
 @task_unique("sync_speaker_volume")
-def sync_volume(target="media_player.living_room.volume_level"):
-    log.warning(kwargs)
-    # for speaker in constants.SPEAKER_GROUP:
+def sync_volume(target="media_player.living_room.volume_level", **kwargs):
+    if not pyscript.media_card.sync:
+        return
+    task.sleep(0.2)
+    if kwargs:
+        target = kwargs["var_name"]
+    volume = state.getattr(target)["volume_level"]
+    for speaker in constants.SPEAKER_GROUP:
+        if speaker != target:
+            media_player.volume_set(entity_id=speaker, volume_level=volume)
 
 
 def group_speakers(target="media_player.living_room"):
