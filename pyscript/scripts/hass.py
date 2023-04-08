f109e26100e4766ec1f0414f03bee451d86ceeb3
papa-marsh <mwarners93@gmail.com>
2023-04-08 15:23:24 -0400

    admin-buttons.yaml & remove system.py

--- pyscript/scripts/hass.py
+++ pyscript/scripts/hass.py
@@ -1,6 +1,11 @@
 import os
 
 
+@time_trigger("cron(0 * * * *)")
+def restart_ssh_addon():
+    hassio.addon_restart(addon="a0d7b954_ssh")
+
+
 @time_trigger("startup")
 def persist_entity_card_hass():
     state.persist(
--- pyscript/scripts/system.py
+++
@@ -1,3 +0,0 @@
-@time_trigger("cron(0 * * * *)")
-def restart_ssh_addon():
-    hassio.addon_restart(addon="a0d7b954_ssh")
